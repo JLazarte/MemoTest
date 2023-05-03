8077418491d598022693575e68772e5db3b53b03
Jlazarte <joselazartelagos@gmail.com>
2023-05-03 04:01:21 -0300

    Fix endGameSession mutation

--- memo-service/app/Models/MemoTest.php
+++ memo-service/app/Models/MemoTest.php
@@ -9,11 +9,6 @@ class MemoTest extends Model
 {
     use HasFactory;
 
-    public function best_session()
-    {
-        return $this->hasMany(GameSession::class);
-    }
-
     public function images()
     {
         return $this->hasMany(MemoTestImage::class);
